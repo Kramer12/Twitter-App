60f2b80b915757aa606bc328a30b08074abf516a
Alex Kramer <alexjk@nc.rr.com>
2012-06-01 11:54:50 -0400

    'restarted'

--- config/initializers/load_hashtag.rb
+++ config/initializers/load_hashtag.rb
@@ -1,2 +1,2 @@
-#raw_settings = File.read(Rails.root + "/config/settings.yml")
-#app_settings = YAML.load(raw_settings)[RAILS_ENV]
+raw_settings = File.read(Rails.root + "/config/settings.yml")
+app_settings = YAML.load(raw_settings)[RAILS_ENV]
