a3200038d4c9c4cf2f1720ce46810196a54b20a9
Alex Kramer <alexjk@nc.rr.com>
2012-06-01 16:36:46 -0400

    'Rails_root'

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Twitterapp::Application.initialize!
 
-APP_CONFIG = YAML::load(File.open("#{RAILS_ROOT}/config/settings.yml"))
+APP_CONFIG = YAML::load(File.open("#{Rails.root}/config/settings.yml"))
