8a5e730e84e180c13cc748830f4ffcae204277ad
Alex Kramer <alexjk@nc.rr.com>
2012-06-01 17:40:57 -0400

    'switch'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
+  def read_settings
+  @tag= APP_CONFIG["hash_tag"]
+  end
   def index
   require 'twitter'
   @tweets = Twitter.search( "#ncstate", :rpp =>20)
-  end
-  def read_settings
-  @tag= APP_CONFIG["hash_tag"]
   end
   
 end
