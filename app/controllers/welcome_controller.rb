6a705c5272799f5fcd53ac9c4d3a826ff06ac7df
Alex Kramer <alexjk@nc.rr.com>
2012-06-01 17:54:02 -0400

    'tag_test'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,8 @@
 class WelcomeController < ApplicationController
-  def read_settings
-  @tag= APP_CONFIG["hash_tag"]
-  end
   def index
   require 'twitter'
-  @tweets = Twitter.search( "tag", :rpp =>20)
+  @tag= APP_CONFIG["hash_tag"]
+  @tweets = Twitter.search( "@tag", :rpp =>20)
   end
   
 end
