538924836fafe2700c279dca5abcbbd33c20cbbd
Alex Kramer <alexjk@nc.rr.com>
2012-06-01 18:06:25 -0400

    'removed'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
   require 'twitter'
   @tag= APP_CONFIG["hash_tag"]
-  @tweets = Twitter.search( "tag", :rpp =>20)
+  @tweets = Twitter.search( @tag, :rpp =>20)
   end
   
 end
