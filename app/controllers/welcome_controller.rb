a82d9e589fa21885ee5f79291adf4e4f266d216a
Alex Kramer <alexjk@nc.rr.com>
2012-06-01 10:33:41 -0400

    'new_test'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   require 'twitter'
-  hash_tag = YAML.load(File.open("#{Rails.root}/config/settings.yml"))
-  @tweets = Twitter.search( "hash_tag.print", :rpp =>20)
+  @tweets = Twitter.search( "#ncstate", :rpp =>20)
   end
 end
 
