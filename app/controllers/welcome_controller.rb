9909edbf72e55c9e286ec3766c5a3b5ca83fd3d5
Alex Kramer <alexjk@nc.rr.com>
2012-06-02 15:11:44 -0400

    'rescent'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
   require 'twitter'
   @tag= APP_CONFIG["hash_tag"]
-  @tweets = Twitter.search( @tag, :include_entities =>1, :rpp =>20)
+  @tweets = Twitter.search( @tag, :include_entities =>1, :rpp =>20).recent
   end
   
 end
